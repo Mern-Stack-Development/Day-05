a08dad9d9ea73ecc3183d4d9ae70b8d88c17ea36
chanaka wickramasinghe <cmwickramasinghe703@gmail.com>
2024-06-02 12:11:52 +0530

    2024 B3

--- Express/cookieParserExample.js
+++ Express/cookieParserExample.js
@@ -8,7 +8,7 @@ app.use(cookieParser());
 
 app.get('/dashboard', (req, res) => {
   const userId = req.cookies.user_name;
-  const emailId = req.cookies.email;
+  const emailId = req.cookies.user_email;
   // Retrieve user data from the database based on the userId
   res.send(`Welcome to the dashboard, User ${userId} with ${emailId}!`);
 });
